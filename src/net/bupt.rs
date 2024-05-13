c06a4e4dd8afd1b709131fab2f4ec98766bf24bc
YouXam <youxam@outlook.com>
2024-05-13 22:28:55 +0800

    Add authentication feature for BUPT portal, untested
    This commit introduces a new feature to authenticate with the BUPT portal. Note that this feature has not yet been tested.
    
    Signed-off-by: YouXam <youxam@outlook.com>

---
+++ src/net/bupt.rs
@@ -0,0 +1,162 @@
+use anyhow::{bail, Result};
+use embedded_svc::http::{client::Client, Method};
+use esp_idf_svc::http::client::{Configuration, EspHttpConnection};
+use log;
+use std::fmt;
+use urlencoding::encode;
+
+macro_rules! fatal {
+    ($($arg:tt)*) => {{
+        let formatted_message = format!($($arg)*);
+        log::error!("{}", formatted_message);
+        bail!(formatted_message);
+    }};
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Clone)]
+pub struct BuptAccount {
+    username: String,
+    password: String,
+}
+
+impl fmt::Debug for BuptAccount {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let password_length = self.password.len();
+        let hidden_password = "*".repeat(password_length);
+        f.debug_struct("BuptAccount")
+            .field("username", &self.username)
+            .field("password", &hidden_password)
+            .finish()
+    }
+}
+
+const CHECK_URL: &str = "http://connect.rom.miui.com/generate_204?cmd=redirect&arubalp=12345";
+
+enum BuptNetStatus {
+    Authenticated,
+    NotAuthenticated(Option<String>),
+}
+
+fn check(url: impl AsRef<str>) -> Result<BuptNetStatus> {
+    log::debug!("checking bupt network status with url: {}", url.as_ref());
+    let connection = EspHttpConnection::new(&Configuration::default())?;
+    let mut client = Client::wrap(connection);
+    let request = client.request(Method::Get, url.as_ref(), &[])?;
+    let response = request.submit()?;
+    log::debug!("response status: {}", response.status());
+    match response.status() {
+        204 => Ok(BuptNetStatus::Authenticated),
+        200..=399 => Ok(BuptNetStatus::NotAuthenticated(
+            response
+                .header("Set-Cookie")
+                .map(|cookie| cookie.split(';').next().map(|cookie| cookie.to_string()))
+                .flatten(),
+        )),
+        _ => fatal!("unexpected status code: {}", response.status()),
+    }
+}
+
+fn auth(account: BuptAccount, cookie: String) -> Result<()> {
+    let connection = EspHttpConnection::new(&Configuration::default())?;
+    let mut client = Client::wrap(connection);
+    let headers = [
+        ("Content-Type", "application/x-www-form-urlencoded"),
+        ("Cookie", &cookie),
+    ];
+    let mut request = client.request(Method::Post, "http://10.3.8.216/login", &headers)?;
+    request.write(
+        format!(
+            "username={}&password={}",
+            encode(&account.username),
+            encode(&account.password)
+        )
+        .as_bytes(),
+    )?;
+    let mut response = request.submit()?;
+    log::debug!("response status: {}", response.status());
+    match response.status() {
+        200 => match check(CHECK_URL)? {
+            BuptNetStatus::Authenticated => {
+                log::info!("BUPT-portal authenticated successfully");
+                Ok(())
+            }
+            _ => {
+                let mut data: Vec<u8> = Vec::new();
+                loop {
+                    let mut buffer = [0u8; 1024];
+                    if let Ok(size) = response.read(&mut buffer) {
+                        if size == 0 {
+                            break;
+                        }
+                        data.extend_from_slice(&buffer[..size]);
+                    } else {
+                        break;
+                    }
+                }
+                let body = String::from_utf8(data)
+                    .map_err(|e| anyhow::anyhow!("failed to parse response body: {}", e))?;
+                let reason = body.find("<div class=\"ui error message\">").map_or(
+                    "Unknown error",
+                    |start| {
+                        &body[start..].find("</div>").map_or("Unknown error", |end| {
+                            &body[(start + 30)..(start + end)].trim()
+                        })
+                    },
+                );
+                fatal!("BUPT-portal authentication failed: {}", reason)
+            }
+        },
+        _ => fatal!("unexpected status code: {}", response.status()),
+    }
+}
+
+pub fn login(account: BuptAccount) -> Result<()> {
+    match check(CHECK_URL) {
+        Ok(BuptNetStatus::Authenticated) => {
+            log::info!("BUPT-portal is already authenticated");
+            Ok(())
+        }
+        Ok(BuptNetStatus::NotAuthenticated(cookie)) => {
+            log::info!(
+                "BUPT-portal not authenticated, authenticating with account: {:?}",
+                account,
+            );
+            auth(
+                account,
+                cookie.map_or_else(
+                    || {
+                        log::warn!("No cookie found in response, may not be able to authenticate");
+                        String::new()
+                    },
+                    |cookie| {
+                        log::info!("Cookie: {}", &cookie);
+                        cookie
+                    },
+                ),
+            )?;
+            Ok(())
+        }
+        Err(e) => fatal!("BUPT-portal status check failed: {}", e),
+    }
+}
+
+#[allow(dead_code)]
+fn get(url: impl AsRef<str>) -> Result<String> {
+    let connection = EspHttpConnection::new(&Configuration::default())?;
+    let mut client = Client::wrap(connection);
+    let request = client.request(Method::Get, url.as_ref(), &[])?;
+    let mut response = request.submit()?;
+    let mut body: Vec<u8> = Vec::new();
+    loop {
+        let mut buffer = [0u8; 1024];
+        if let Ok(size) = response.read(&mut buffer) {
+            if size == 0 {
+                break;
+            }
+            body.extend_from_slice(&buffer[..size]);
+        } else {
+            break;
+        }
+    }
+    Ok(String::from_utf8(body).unwrap_or_else(|_| "".to_string()))
+}
--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,8 +1,11 @@
+mod bupt;
+
 use anyhow::{bail, Result};
 use esp_idf_hal::delay;
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
     hal::{peripheral, prelude::Peripherals},
+    log::set_target_level,
     wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi},
 };
 use log::info;
@@ -13,8 +16,12 @@ fn connect_wifi(
     modem: impl peripheral::Peripheral<P = esp_idf_svc::hal::modem::Modem> + 'static,
     sysloop: EspSystemEventLoop,
 ) -> Result<Box<EspWifi<'static>>> {
+    let mut bupt_account = None;
     let (auth_method, ssid, pass) = match config {
-        NetConfig::BuptPortal(_) => (AuthMethod::None, "BUPT-portal".to_string(), String::new()),
+        NetConfig::BuptPortal(account) => {
+            bupt_account = Some(account);
+            (AuthMethod::None, "BUPT-portal".to_string(), String::new())
+        }
         NetConfig::NormalWifi(wifi) => (AuthMethod::WPA2Personal, wifi.ssid, wifi.password),
     };
     let mut esp_wifi = EspWifi::new(modem, sysloop.clone(), None)?;
@@ -29,11 +36,8 @@ fn connect_wifi(
     }))?;
 
     info!("Starting wifi...");
-
     wifi.start()?;
-
     info!("Connecting wifi {}...", ssid);
-
     let delay: delay::Delay = Default::default();
 
     for retry in 0..10 {
@@ -50,31 +54,16 @@ fn connect_wifi(
     }
 
     info!("Waiting for DHCP lease...");
-
     wifi.wait_netif_up()?;
 
     let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
-
     info!("Wifi DHCP info: {:?}", ip_info);
 
-    Ok(Box::new(esp_wifi))
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Clone)]
-struct BuptAccount {
-    username: String,
-    password: String,
-}
-
-impl fmt::Debug for BuptAccount {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let password_length = self.password.len();
-        let hidden_password = "*".repeat(password_length);
-        f.debug_struct("BuptAccount")
-            .field("username", &self.username)
-            .field("password", &hidden_password)
-            .finish()
+    if let Some(account) = bupt_account {
+        bupt::login(account)?;
     }
+
+    Ok(Box::new(esp_wifi))
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Clone)]
@@ -96,11 +85,14 @@ impl fmt::Debug for Wifi {
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 enum NetConfig {
-    BuptPortal(BuptAccount),
+    BuptPortal(bupt::BuptAccount),
     NormalWifi(Wifi),
 }
 
 pub fn connect() -> anyhow::Result<()> {
+    set_target_level("wifi", log::LevelFilter::Warn)?;
+    set_target_level("wifi_init", log::LevelFilter::Warn)?;
+
     let config: NetConfig = match crate::nvs::load::<NetConfig>()? {
         Some(config) => {
             log::info!("Loaded NetConfig: {:?}", &config);
