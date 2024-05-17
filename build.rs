23674eb2cce150e416ae76f3110771a136f494cd
YouXam <youxam@outlook.com>
2024-05-18 00:00:24 +0800

    feat: enhance build system to automatically build and embed frontend artifacts
    Signed-off-by: YouXam <youxam@outlook.com>

--- build.rs
+++ build.rs
@@ -1,3 +1,41 @@
+use std::env;
+use std::fs;
+use std::io;
+use std::path::Path;
+use std::process::Command;
+
+fn copy_dir(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+    fs::create_dir_all(&dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            println!("cargo:rerun-if-changed={}", entry.path().to_str().unwrap());
+            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
+}
+
 fn main() {
     embuild::espidf::sysenv::output();
+
+    let status = Command::new("pnpm")
+        .args(["run", "build"])
+        .current_dir("frontend")
+        .status()
+        .expect("Failed to build frontend project");
+
+    if !status.success() {
+        panic!("Frontend build failed");
+    }
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    copy_dir(
+        Path::new("frontend/dist"),
+        Path::new(&out_dir).join("frontend"),
+    )
+    .expect("Failed to copy frontend dist directory");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod net;
 mod nvs;
 
+const INDEX_HTML: &str = include_str!(concat!(env!("OUT_DIR"), "/frontend/index.html"));
+
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
+
+    log::info!("{}", INDEX_HTML);
 
     net::connect()?.stop()?;
 
